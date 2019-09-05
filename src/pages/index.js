cf5220e68281efb017805102241419c8d8fdbea0
Oleg Lytvyn <igocooper@gmail.com>
2019-09-05 22:34:24 +0300

    clean index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,8 @@
-import React, {useState} from "react"
-import { Link } from "gatsby"
+import React from "react"
 
 import Layout from "../components/layout"
 import Products from "../components/products"
 import SEO from "../components/seo"
-
-const tankTopSkus = [
-  {
-    value: "sku_FkvpxV8wOa0F7o",
-    label: "Small"
-  },
-  {
-    value: "sku_FkvpMm6IRMog6S",
-    label: "Medium"
-  },
-  {
-    value: "sku_FkvpYhiSqdhUOC",
-    label: "Large"
-  },
-];
-
-const tShirtSkus = [
-  {
-    value: "sku_Fkvf05b8rndQ2Q",
-    label: "Small"
-  },
-  {
-    value: "sku_FkvfoMbNTonOlk",
-    label: "Medium"
-  },
-  {
-    value: "sku_FkAz3SHc1exnSA",
-    label: "Large"
-  },
-];
 
 const IndexPage = () => {
   return (
